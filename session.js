40a7abfc34204e0fbb9644ac45f8a51f6ad30039
Ameshsandipa <REALKINGAMESHSANDIPA@gmail.com>
2024-12-02 16:59:08 +0530

    Update session.js

--- session.js
+++ session.js
@@ -21,5 +21,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session_id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=PN9yVSpB#wlSz4vNAFZpB5M4AmZl7tGTsn6hzJ_5Hxnxeqp2Lj1Y",
 };
